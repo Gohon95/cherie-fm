92829de8e19815b2f4cd57ec05bd262e54a77b9a
Gohon95 <axel.gonon@hotmail.fr>
2022-06-09 15:55:17 +0200

    ajout contenu et service worker

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
 import React from 'react';
-import { FormControl, Nav, Navbar } from 'react-bootstrap';
+import { Card, FormControl, Nav, Navbar, Button, Row, Col, Container } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
 
 function App() {
   return (
-    <div class="container">
-    <div class="row">
-      <div class="col">
+    <Container>
       <Navbar bg="warning" variant="dark">
-      <img href="../src/images/logo.png"></img>
-      <div class="col">
       <Nav className="mr-auto">
         <Nav.Link href="#home">Profile</Nav.Link>
         </Nav>
-        </div>
+        <img class="logo" src="/images/logo.png"/>
     </Navbar>
-      </div>
-    </div>
-    </div>
+  <Row className="justify-content-center">
+    <Col xs lg="2">
+    <iframe class="align-items-center col-12 mt-5 video" src="https://www.youtube.com/embed/zpOULjyy-n8?rel=0" allowfullscreen></iframe>
+    </Col>
+  </Row>
+  <Row className="justify-content-center">
+  <img src="/images/image1.png"/>
+  </Row>
+  <Row className="justify-content-center">
+    <Col xs lg="2">
+    <iframe class="align-items-center col-6 video-card" src="https://www.youtube.com/embed/zpOULjyy-n8?rel=0" allowfullscreen></iframe>
+    </Col>
+    <Col xs="4" lg="2">
+    <iframe class="align-items-center col-6 video-card" src="https://www.youtube.com/watch?v=J8uyhOvGAgo" allowfullscreen></iframe>
+    </Col>
+    <Button xs lg="2" variant="warning">Voir plus</Button>
+  </Row>
+</Container>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -4,17 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import PWAPrompt from 'react-ios-pwa-prompt';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-    <PWAPrompt copyTitle="PWA Tutorial"/>
   </React.StrictMode>
 );
 
-ServiceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
